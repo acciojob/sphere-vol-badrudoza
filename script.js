55170d354f9b3b9a72429afb2280a70a3eb6ba88
badrudoza <106438583+badrudoza@users.noreply.github.com>
2025-07-08 04:20:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function volume_sphere(event) {
     const radiusinp=document.getElementById("radius").value;
 	const radius=parseInt(radiusinp);
 	if(isNaN(radius)) {
-		document.getElementById("volume").value="Calculated volume should be 'NaN' due to invalid input."
+		document.getElementById("volume").value="NaN";
 		return;
 	}
 	let volume=(4/3)*Math.PI*Math.pow(radius,3);
-	document.getElementById("volume").value="Calculated volume should be approximately "+volume.toFixed(4)+".";
+	document.getElementById("volume").value= volume.toFixed(4);
 	
   
 } 
